ddb71187b64afc27e2e5f25bc9608cceca9b95fd
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-03-21 15:05:14 +0600

    add coffee api created

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
 const port = process.env.port || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-// restart-coffee-crud
-// rtGeRYJhO48WK6mJ
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://restart-coffee-crud:<db_password>@cluster0.qvjjrvn.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qvjjrvn.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,12 +24,25 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        const coffeeCollection = client.db('Espresso_Emporium_Coffee_Db').collection('Coffee');
+
+        app.post('/coffee', async (req, res) => {
+            const newCoffee = req.body;
+            // console.log('Adding new coffee', newCoffee)
+
+            const result = await coffeeCollection.insertOne(newCoffee);
+            res.send(result);
+        });
+
+
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
